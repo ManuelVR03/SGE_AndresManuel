ceec0a1942a9b75b28429191bdb0598f6ccc15ce
Andresteruel <andres.teruel.torres.alu@iesjulioverne.es>
2025-01-10 14:06:49 +0100

    version final

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -14,41 +14,59 @@ public class Calculadora {
         System.out.println("--------   Resta (1)  ----------");
         System.out.println("------  Multiplicación (2)  ----");
         System.out.println("-------   División (3)  --------");
-        System.out.println("------  Raiz Cuadrada (4)  -----");
+        System.out.println("-------   Potencia (4)  --------");
+        System.out.println("------  Raiz Cuadrada (5)  -----");
         System.out.println("--------------------------------");
         System.out.println();
-        
+
+        Operaciones op = new Operaciones();
+        double numero1 = 0, numero2 = 0, resultado = 0;
+
         do {
             inc = false;
             try {
                 System.out.print("Introduzca una opción: ");
                 respuesta = tecInt.nextInt();
 
-                switch (respuesta) {
-                    case 0:
-                        break;
+                if (respuesta == 5) {
+                    System.out.println("Introduzca el número: ");
+                    numero1 = tecInt.nextInt();
+                    resultado = op.raizCuadrada(numero1);
+                } else if (respuesta >= 0 && respuesta <= 4) {
+                    System.out.println("Introduzca el primer número: ");
+                    numero1 = tecInt.nextInt();
 
-                    case 1:
-                        break;
+                    System.out.println("Introduzca el segundo número: ");
+                    numero2 = tecInt.nextInt();
 
-                    case 2:
-                        break;
+                    switch (respuesta) {
+                        case 0:
+                            resultado = op.suma(numero1, numero2);
+                            break;
+                        case 1:
+                            resultado = op.resta(numero1, numero2);
+                            break;
+                        case 2:
+                            resultado = op.multiplicacion(numero1, numero2);
+                            break;
+                        case 3:
+                            resultado = op.division(numero1, numero2);
+                            break;
+                        case 4:
+                            resultado = op.potencia(numero1, numero2);
+                            break;
+                    }
 
-                    case 3:
-                        break;
-
-                    case 4:
-                        break;
-
-                    default:
-                        System.out.println("La opción introducida no se encuentra en el menú.");
-                        break;
+                } else {
+                    System.out.println("La opción introducida no se encuentra en el menú.");
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Opción inválida. Por favor, introduzca un número dentro del rango.");
+                System.out.println("Opción inválida. Por favor, introduzca un número válido.");
                 tecInt.next();
                 inc = true;
             }
-        } while (respuesta >= 0 && respuesta <= 4 && inc);
+        } while (respuesta >= 0 && respuesta <= 5 && inc);
+        System.out.println("Resultado: "+resultado);
+
     }
 }
